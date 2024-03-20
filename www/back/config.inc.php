e051abce78c822a828b56dc2e89600ee4c253784
guillermo <guillermo@localhost.localdomain>
2024-03-20 19:25:14 -0300

    Fix Dockerfile
    	modified:   Dockerfile-apache
    	modified:   apache/vhost/scada2.conf
    	new file:   composer-installer.php
    	modified:   deploy.txt
    	modified:   docker-compose.yml
    	new file:   www/back/config.inc.php
    	renamed:    www/back/src/funciones.inc.php -> www/back/funciones.inc.php
    	renamed:    www/back/src/lecturas.php -> www/back/lecturas.php
    	deleted:    www/back/src/config.inc.php
    	modified:   www/scada2/public/build/assets/app-6bb7321a.css
    	renamed:    www/scada2/public/build/assets/app-ef07b373.js -> www/scada2/public/build/assets/app-96bec1a2.js
    	modified:   www/scada2/public/build/assets/app-a71e5603.css
    	modified:   www/scada2/public/build/manifest.json

---
+++ www/back/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+define('SERVER_IP', 'mariadb');
+define('USERNAME', 'root');
+define('PASSWORD', '12345678');
+define('MYSQL_DATABASE', 'scada2'); 
--- www/back/src/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('SERVER_IP', 'mariadb');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('MYSQL_DATABASE', 'paranamedio'); 
